37457da94523688be8246aa6cfc1248296d1041a
Ivy Wong <ivy@ivywong.dev>
2024-01-07 13:48:59 -0800

    Add Atom feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 const markdownIt = require("markdown-it");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss);
+  
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/res");
   eleventyConfig.addWatchTarget("./src/css/");
